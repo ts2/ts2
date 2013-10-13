69db8f8378391529b51b53280a5fb60b7fbd36eb
Nicolas Piganeau <np@obp.fr>
2013-10-13 16:32:28 +0200

    Add cx_freeze setup script

---
+++ setup.py
@@ -0,0 +1,26 @@
+# cx_freeze setup file
+
+import sys
+
+from cx_Freeze import setup, Executable
+
+build_exe_options = {
+    "includes" : ["atexit"],
+    "packages" : ["re"],
+    "include_files" : [
+        ("data/drain.ts2","simulations/drain.ts2"),
+        ("data/liverpool-st.ts2","simulations/liverpool-st.ts2")],
+    "icon" : "images/ts2.ico"}
+
+base = None
+
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+setup(
+        name = "ts2",
+        version = "0.3",
+        description = "Train Signalling Simulation",
+        options = {"build_exe" : build_exe_options},
+        executables = [Executable("ts2.py", base = base)])
+
