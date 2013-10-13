6b180cc9237126862ebba4d5972f4edacb99443e
Nicolas Piganeau <np@obp.fr>
2013-10-13 18:58:28 +0200

    Small fix on liverpool-st sim

--- setup.py
+++ setup.py
@@ -9,7 +9,8 @@ build_exe_options = {
     "packages" : ["re"],
     "include_files" : [
         ("data/drain.ts2","simulations/drain.ts2"),
-        ("data/liverpool-st.ts2","simulations/liverpool-st.ts2")],
+        ("data/liverpool-st.ts2","simulations/liverpool-st.ts2"),
+        ("README.md","doc/README.txt")],
     "icon" : "images/ts2.ico"}
 
 base = None
