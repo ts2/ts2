e12af5ed98f360cb86325ecbfb1d17e7bdb4b52d
Nicolas Piganeau <nicolas.piganeau@ndp-systemes.fr>
2019-06-04 23:04:16 +0200

    Updated release files

--- build.py
+++ build.py
@@ -15,12 +15,13 @@ def find_file(name, path):
             return os.path.join(root, name)
 
 print("############# Generating translation files ###################")
-out = os.system("lrelease i18n/ts2.pro")
+# out = os.system("lrelease i18n/ts2.pro")
 
 print("############# Freezing TS2 as an executable ###################")
 build_exe_options = {
     "includes": ["atexit"],
-    "packages": ["re"],
+    "excludes": ["tkinter", "test"],
+    "packages": ["re", "simplejson", "websocket"],
     "include_files": [
         ("README.md", "doc/README.txt"),
         ("README_fr.md", "doc/README_fr.txt"),
@@ -31,7 +32,7 @@ build_exe_options = {
         ("data/README", "data/README"),
     ],
     "include_msvcr": True,
-    "icon": "images/ts2.ico"}
+}
 
 base = None
 if sys.platform == "win32":
@@ -42,7 +43,7 @@ setup(
     version=__VERSION__,
     description=__APP_LONG__,
     options={"build_exe": build_exe_options},
-    executables=[Executable("start-ts2.py", base=base, targetName="ts2.exe")]
+    executables=[Executable("start-ts2.py", base=base, targetName="ts2.exe", icon="images/ts2.ico")]
 )
 
 if sys.platform == "win32":
